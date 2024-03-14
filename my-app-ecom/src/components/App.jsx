75e70802b7cbf26b703adcc981f9e627fa79a5b9
Anna Wojtowicz <annwoj26185@stud.noroff.no>
2024-03-14 15:41:14 +0100

    Refactor createCard function and update product rendering

--- my-app-ecom/src/components/App.jsx
+++ my-app-ecom/src/components/App.jsx
@@ -2,29 +2,27 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Product from './Product';
-import Price from './Price';
 import { getProducts } from '../api';
 
 const products = await getProducts()
 const productsData = products.data;
 
-function createCard(item) {
-  return (
+// function createCard(item) {
+//   return (
+//     <Product className="card"
+//       key={item.id}
+//       title={item.title}
+//       description={item.description}
+//       price={item.price}
+//       discountedPrice={item.discountedPrice}
+//       // image={item.image}
+//       rating={item.rating}
+//       tags={item.tags}
+//     //reviews={item.reviews}
+//     />
 
-    <Product className="card"
-      key={item.id}
-      title={item.title}
-      description={item.description}
-      price={item.price}
-      discountedPrice={item.discountedPrice}
-      // image={item.image}
-      rating={item.rating}
-      tags={item.tags}
-    //reviews={item.reviews}
-    />
-
-  );
-}
+//   );
+// }
 
 function App() {
 
@@ -32,7 +30,21 @@ function App() {
     <div>
       <Header />
       <div className=''>
-        {productsData.map(createCard)}
+        {productsData.map(item => (
+          <Product className="card"
+            key={item.id}
+            title={item.title}
+            description={item.description}
+            price={item.price}
+            discountedPrice={item.discountedPrice}
+            // image={item.image}
+            rating={item.rating}
+            tags={item.tags}
+          //reviews={item.reviews}
+          />
+
+        )
+        )}
       </div>
       <Footer />
     </div>
