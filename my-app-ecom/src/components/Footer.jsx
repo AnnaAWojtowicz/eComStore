785a565b831a9c0f45a3637e285b3a7cb89191cf
Anna Wojtowicz <annwoj26185@stud.noroff.no>
2024-03-18 16:25:53 +0100

    Add styling and functionality to footer and form

--- my-app-ecom/src/components/Footer.jsx
+++ my-app-ecom/src/components/Footer.jsx
@@ -3,8 +3,10 @@ import React from "react";
 function Footer() {
     const year = new Date().getFullYear();
     return (
-        <footer>
+        <footer className="footer">
+            <div className="bungee-outline-regular footerLogo">Ecom</div>
             <p>Copywright {year}</p>
+            <div>Contact</div>
         </footer>
     );
 }
