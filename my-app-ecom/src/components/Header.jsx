2458369a9678f9a8b6edd445318d22e3079dfff4
Anna Wojtowicz <annwoj26185@stud.noroff.no>
2024-03-18 15:58:25 +0100

    Add new font and update header styles

--- my-app-ecom/src/components/Header.jsx
+++ my-app-ecom/src/components/Header.jsx
@@ -3,13 +3,16 @@ import React from "react";
 function Header() {
     return (
         <header className="header">
-            <h1>Ecom</h1>
+            <div className="bungee-outline-regular">Ecom</div>
             <div className="searchContainer">
                 <input type="text" className="searchInput" placeholder="Search">
                 </input>
             </div>
-            <div className="material-symbols-outlined">shopping_bag</div>
-        </header >
+            <div className="CartAndNumber">
+                <span className="material-symbols-outlined exceptionIcon">shopping_bag</span>
+                <span className="numberOfItems">6</span>
+            </div>
+        </header>
     );
 }
 
